d2c6b09786fb6d145ecf2adc239123f5ca0d6893
2ty9p66r46-del <2ty9p66r46@privaterelay.appleid.com>
2026-09-16 04:55:27 +0900

    Stabilize understanding filter button sizing

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='flovo-v71';
+const CACHE='flovo-v72';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./flovo-wordmark-v3.png','./Import-data_format.xlsx'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))])));
