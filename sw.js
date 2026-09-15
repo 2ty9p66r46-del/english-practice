22fd26a7b527d030651eaaa90b0117d622bbb597
2ty9p66r46-del <2ty9p66r46@privaterelay.appleid.com>
2026-09-15 15:43:52 +0900

    Cache exact FloVo wordmark asset

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='flovo-v5';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+const CACHE='flovo-v6';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./flovo-wordmark.svg'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))])));
 self.addEventListener('fetch',event=>{if(event.request.method==='GET')event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))});
