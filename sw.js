209829bfb96d1398dedfdebab39b33de0d5e89fd
2ty9p66r46-del <2ty9p66r46@privaterelay.appleid.com>
2026-09-15 23:44:55 +0900

    Shift filter palette to yellow green and blue

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='flovo-v37';
+const CACHE='flovo-v38';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./flovo-wordmark-v2.png','./Import-data_format.xlsx'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))])));
