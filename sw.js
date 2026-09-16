2d0900a8c54d8056e8977c4038f582a14e9205c3
2ty9p66r46-del <2ty9p66r46@privaterelay.appleid.com>
2026-09-16 17:47:07 +0900

    キャッシュをv122へ更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='flovo-v121';
+const CACHE='flovo-v122';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','./flovo-wordmark-v3.png','./Import-data_format.xlsx'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))])));
